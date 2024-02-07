3306517d51b7b1dfd98c8f4e04a2c50d228c4a47
maheshmeenabalot <128252442+maheshmeenabalot@users.noreply.github.com>
2024-02-08 00:33:02 +0530

    Add files via upload

---
+++ Backend/index.js
@@ -0,0 +1,43 @@
+var express = require("express");
+var app = express();
+var cors= require("cors");
+app.use(cors());
+
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+// app.use(express.json());
+
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host : 'localhost',
+    database: 'College',
+    user : 'root',
+    password: '6350'
+});
+connection.connect(function(err) {
+    if (err) {
+        console.error('Error connecting: ' + err.stack);
+        return;
+    }
+    console.log('Connected as id ' + connection.threadId);
+});
+    app.get("/",(req,res)=>{
+        res.send("working");
+    });
+    
+    app.get("/details", (req,res)=>{
+        
+        connection.query('SELECT * FROM student', function async (err, results, fields) {
+            res.json(results);
+        });
+    });
+
+
+
+var port = process.env.port || 5000
+app.listen(port, function (req, res) {
+    console.log(`server started at port ${port}`);
+});
+
