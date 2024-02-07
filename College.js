00b59e716dfef91cee3b61ce5c0879f7be4a3620
maheshmeenabalot <128252442+maheshmeenabalot@users.noreply.github.com>
2024-02-07 12:16:56 +0530

    Add files via upload

---
+++ College.js
@@ -0,0 +1,24 @@
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host : 'localhost',
+    database: 'College',
+    user : 'root',
+    password: '5050'
+});
+
+connection.connect(function(err) {
+    if (err) {
+        console.error('Error connecting: ' + err.stack);
+        return;
+    }
+
+    console.log('Connected as id ' + connection.threadId);
+});
+
+connection.query('SELECT * FROM student', function (error, results, fields) {
+    if (error)
+        throw error;
+    results.forEach(result => {
+        console.log(result);
+    });
+});
