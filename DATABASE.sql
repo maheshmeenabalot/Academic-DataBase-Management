217758397f733910a324cb7d18838c3b8a2ab687
Aryan <84695627+aryanolkha@users.noreply.github.com>
2024-02-28 00:38:14 +0530

    CGPA added.sql

--- DATABASE.sql
+++ DATABASE.sql
@@ -9,7 +9,8 @@ CREATE TABLE IF NOT EXISTS student (
   Branch varchar(50) NOT NULL,
   Batch INT NOT NULL,
   Gender varchar(10) NOT NULL,
-  Email varchar(50) NOT NULL);
+  Email varchar(50) NOT NULL,
+  CGPA FLOAT(4));
 
 ----- Creating a table to store course details within the database-----
 CREATE TABLE IF NOT EXISTS courses (
@@ -19,14 +20,14 @@ CREATE TABLE IF NOT EXISTS courses (
   Email varchar(50) NOT NULL);
 
 ----- Inserting data into student table-----
-INSERT INTO student (Student_ID, Student_Name, Branch, Batch, Gender, Email) VALUES
-('2003120',	'Mahesh', 'CSE', '2020', 'Male', 'mahesh.meena.20031@iitgoa.ac.in'),
-('2003306',	'Aryan', 'MNC', '2020',	'Male', 'aryan.olkha.20033@iitgoa.ac.in'),
-('2003319',	'Rajesh', 'MNC', '2020', 'Male', 'rajesh.meena.20033@iitgoa.ac.in'),
-('2004220', 'Mesh', 'EE', '2020', 'Male', 'mesh.meena.20042@iitgoa.ac.in'),
-('2006319',	'Jesh',	'ME', '2020', 'Female',	'jesh.ena.20063@iitgoa.ac.in'),
-('2103116',	'Ryan',	'CSE',	'2021',	'Male',	'ryan.khan.21031@iitgoa.ac.in'), 
-('2104214', 'Mitu', 'EE', '2021', 'Female', 'mitu.20042@iitgoa.ac.in');
+INSERT INTO student (Student_ID, Student_Name, Branch, Batch, Gender, Email, CGPA) VALUES
+('2003120',	'Mahesh', 'CSE', '2020', 'Male', 'mahesh.meena.20031@iitgoa.ac.in', 7.75),
+('2003306',	'Aryan', 'MNC', '2020',	'Male', 'aryan.olkha.20033@iitgoa.ac.in', 7.50),
+('2003319',	'Rajesh', 'MNC', '2020', 'Male', 'rajesh.meena.20033@iitgoa.ac.in', 9.92),
+('2004220', 'Mesh', 'EE', '2020', 'Male', 'mesh.meena.20042@iitgoa.ac.in', 5.44),
+('2006319',	'Jesh',	'ME', '2020', 'Female',	'jesh.ena.20063@iitgoa.ac.in', 8.45),
+('2103116',	'Ryan',	'CSE',	'2021',	'Male',	'ryan.khan.21031@iitgoa.ac.in', 6.49 ), 
+('2104214', 'Mitu', 'EE', '2021', 'Female', 'mitu.20042@iitgoa.ac.in', 6.98);
 
 ----- Inserting data into courses table-----
 INSERT INTO courses (Course_code, School, Instructor_Name, Email) VALUES
