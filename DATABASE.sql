b7756c77acdbe1091b0c3b421408579d74cc7e1a
Aryan <84695627+aryanolkha@users.noreply.github.com>
2024-02-28 11:26:45 +0530

    Update

--- DATABASE.sql
+++ DATABASE.sql
@@ -3,7 +3,8 @@ CREATE DATABASE College;
 USE College;
 
 ----- Creating a table to store student details within the database-----
-CREATE TABLE IF NOT EXISTS student (
+DROP TABLE IF EXISTS students;
+CREATE TABLE IF NOT EXISTS students (
   Student_ID INT PRIMARY KEY NOT NULL,
   Student_Name varchar(50) NOT NULL,
   Branch varchar(50) NOT NULL,
@@ -13,6 +14,7 @@ CREATE TABLE IF NOT EXISTS student (
   CGPA FLOAT(4));
 
 ----- Creating a table to store course details within the database-----
+DROP TABLE IF EXISTS courses;
 CREATE TABLE IF NOT EXISTS courses (
   Course_Code varchar(50) PRIMARY KEY NOT NULL,
   School varchar(50) NOT NULL,
@@ -20,7 +22,7 @@ CREATE TABLE IF NOT EXISTS courses (
   Email varchar(50) NOT NULL);
 
 ----- Inserting data into student table-----
-INSERT INTO student (Student_ID, Student_Name, Branch, Batch, Gender, Email, CGPA) VALUES
+INSERT INTO students (Student_ID, Student_Name, Branch, Batch, Gender, Email, CGPA) VALUES
 ('2003120', 'Mahesh', 'CSE', '2020', 'Male', 'mahesh.meena.20031@iitgoa.ac.in', 8.75),
 ('2003306', 'Aryan', 'MNC', '2020',	'Male', 'aryan.olkha.20033@iitgoa.ac.in', 7.50),
 ('2003319', 'Rajesh', 'MNC', '2020', 'Male', 'rajesh.meena.20033@iitgoa.ac.in', 9.92),
@@ -40,3 +42,20 @@ INSERT INTO courses (Course_code, School, Instructor_Name, Email) VALUES
 ('ME606', 'SMS', 'Dr. Priya', 'priya@iitgoa.ac.in'),
 ('ME609', 'SMS', 'Dr. Arindam', 'arindam@iitgoa.ac.in');
 
+DROP TABLE IF EXISTS course_enrollment;
+CREATE TABLE IF NOT EXISTS course_enrollment (
+	Enrollment_ID INT AUTO_INCREMENT,
+    Course_Code VARCHAR(50),
+    Student_ID INT,
+    FOREIGN KEY (Course_Code) REFERENCES courses(Course_Code),
+    FOREIGN KEY (Student_ID) REFERENCES students(Student_ID),
+    PRIMARY KEY (Enrollment_ID, Course_Code, Student_ID));
+    
+INSERT INTO course_enrollment (Course_code, Student_ID) VALUES 
+('CS101', '2103116'),
+('CS101', '2003121'),
+('CS441', '2003120'),
+('CS441', '2003306'),
+('CS441', '2003319'),
+('CS441', '2003121'),
+('EE102', '2004220');
